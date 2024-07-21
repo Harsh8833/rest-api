e56266fe79863bee09d09fb88167b8de5a9698fe
harsh8833 <harsh.raj8833@gmail.com>
2024-07-22 01:50:53 +0530

    added: Dockerfile

--- src/api/index.js
+++ src/api/index.js
@@ -10,13 +10,13 @@ postgres.query(`CREATE TABLE IF NOT EXISTS clicks (
   created_at TIMESTAMP DEFAULT NOW()
 )`)
 
-app.get('/api/count', (req, res) => {
+app.get('api/count', (req, res) => {
   postgres.query('SELECT count(*) AS count FROM clicks', (err, resp) => {
     res.send({ count: resp.rows[0].count || 0 })
   })
 })
 
-app.post('/api/count/increment', (req, res) => {
+app.post('api/count/increment', (req, res) => {
   postgres.query('INSERT INTO clicks DEFAULT VALUES', (err, insert) => {
     postgres.query('SELECT count(*) AS count FROM clicks', (err, resp) => {
       res.send({ count: resp.rows[0].count || 0 })
--- src/www/App.js
+++ src/www/App.js
@@ -8,13 +8,13 @@ class App extends Component {
   }
 
   componentDidMount = async () => {
-    const { count } = await window.fetch(`/api/count`).then(res => res.json())
+    const { count } = await window.fetch(`api/count`).then(res => res.json())
     this.setState({ count })
   }
 
   increment = async () => {
     const { count } = await window
-      .fetch(`/api/count/increment`, { method: 'POST' })
+      .fetch(`api/count/increment`, { method: 'POST' })
       .then(res => res.json())
     this.setState({ count })
   }
